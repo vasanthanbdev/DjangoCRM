98c966e1a7670b68c1b0a5890afafa93ec69ae5d
vasanthanb <vasanthan_b@outlook.com>
2023-09-10 16:49:46 +0530

    login-registeration

--- crm/urls.py
+++ crm/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    # path('/login', views.log_in, name="login"),
+    path('/log_out', views.log_out, name="logout"),
+    path('/register', views.register_user, name="register"),
 ]
--- crm/views.py
+++ crm/views.py
@@ -1,4 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm
 
 def home(request):
-    return render(request, "home.html")
+    #if user is trying to log in 
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        #authentication
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Successfully Logged In!")
+            return redirect('home')
+        else:
+            messages.success(request, "There Was An Error Loggin You In, Please Try Again...")
+            return redirect('home')
+    else:
+        return render(request, "home.html")
+
+# def log_in(request):
+#     pass
+
+def log_out(request):
+    logout(request)
+    messages.success(request, "You Have Been Successfully Logged Out...")
+    return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #authenticate and login
+            username = form.changed_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Been Registered Successfully...")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, "register.html", {'form': form})
