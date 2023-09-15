755aa6252a10ae719d6925b987801bc4eca30cae
vasanthanb <vasanthan_b@outlook.com>
2023-09-15 13:42:21 +0530

    record

--- crm/urls.py
+++ crm/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     # path('/login', views.log_in, name="login"),
-    path('/log_out', views.log_out, name="logout"),
-    path('/register', views.register_user, name="register"),
+    path('log_out/', views.log_out, name="logout"),
+    path('register/', views.register_user, name="register"),
+    path('record/<int:pk>/', views.customer_record, name="customer_record"),
 ]
--- crm/views.py
+++ crm/views.py
@@ -47,3 +47,11 @@ def register_user(request):
     else:
         form = SignUpForm()
         return render(request, "register.html", {'form': form})
+    
+def customer_record(request, pk):
+    if request.user.is_authenticated :
+        customer_record = Record.objects.get(id=pk)
+        return render(request, "record.html", {"customer_record": customer_record})
+    else:
+        messages.success(request, "You are not logged in...")
+        return redirect('home')
